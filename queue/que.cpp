494117333d2753b63f28d9cef30ccb3a1302ddaf
ashnafi1426 <ashenafisileshi7@gmail.com>
2025-05-25 12:00:34 +0300

    update all

--- queue/que.cpp
+++ queue/que.cpp
@@ -1,5 +1,17 @@
 #include<iostream>
 using namespace std;
+#include<queue>
 int main(){
-  
+queue<int> myqueue;
+myqueue.push(1);
+myqueue.push(2);
+myqueue.push(3);
+myqueue.push(4);
+cout<<"size is "<<myqueue.size()<<endl;
+cout<<"first element is"<<myqueue.front()<<endl;
+
+cout<<"last element is"<<myqueue.back()<<endl;
+printQueue();
+return 0;
+
 }
