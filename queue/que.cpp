8e71a628f14bdad1a87e2fe0ca16f995bcbb5b4f
ashnafi1426 <ashenafisileshi7@gmail.com>
2025-05-25 12:03:42 +0300

    priter

--- queue/que.cpp
+++ queue/que.cpp
@@ -6,10 +6,9 @@ queue<int> myqueue;
 myqueue.push(1);
 myqueue.push(2);
 myqueue.push(3);
-myqueue.push(4);
-cout<<"size is "<<myqueue.size()<<endl;
-cout<<"first element is"<<myqueue.front()<<endl;
-
+myqueue.push(499999;
+cout<<"size is "<<myqueue.size()<<endl
+cout<<"first element is"<<myqueue.front()<<endl
 cout<<"last element is"<<myqueue.back()<<endl;
 printQueue();
 return 0;
