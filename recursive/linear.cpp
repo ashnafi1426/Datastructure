2ea1f5e4a779aa8cafe402b087b360be003e00ed
ashnafi1426 <ashenafisileshi7@gmail.com>
2025-05-26 00:39:27 +0300

    crete all

---
+++ recursive/linear.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int linearSearchh(int arr[],int size,int target){
+  for(int i=0;i<n;++i){
+  if(arr[i]==target){
+    return i;
+  }else{
+    return -1;
+  }
+}
+}
+int main(){
+  int arr[]={1,2,3,3,4,5,5};
+  int size=sizeof(arr);
+  int searchelement=4;
+  int result=linearSearch(arr,size,searchelement)
+  if
+}
